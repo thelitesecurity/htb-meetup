37f6650fc652201c27ca0ce82bade58141fcfbff
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2025-05-18 16:59:10 +0000

    update

--- run01.sh
+++ run01.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-install_dir="/goinfre/elitesec/"
+install_dir="/home/elitesec/"
 archive_file="kali-linux-2025.1c-qemu-amd64.7z"
-kali_qcow="/goinfre/elitesec/kali-linux-2025.1c-qemu-amd64.qcow2"
+kali_qcow="${install_dir}/kali-linux-2025.1c-qemu-amd64.qcow2"
 
 if [ ! -f $kali_qcow ]; then
     if [ ! -f $archive_file ]; then
